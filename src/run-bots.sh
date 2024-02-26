5998e4ad26e542e6e388f52d42f5a564e5ba7704
EleanorCordelia <bryan.rich0604@gmail.com>
2024-02-26 17:24:05 +0700

    feat: upload file starter and project template

---
+++ src/run-bots.sh
@@ -0,0 +1,5 @@
+
+python3 main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo --host http://172.26.160.1:8082/api &
+python3 main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo --host http://172.26.160.1:8082/api &
+python3 main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo --host http://172.26.160.1:8082/api &
+python3 main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo --host http://172.26.160.1:8082/api &
